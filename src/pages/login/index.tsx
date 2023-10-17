2536cefeea96ffc7e424e409cae69b008ca4d4c3
[your <pradita397@gmail.com>
2023-10-17 23:54:15 +0700

    Add simple login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import styles from "./Login.module.scss";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      setError("");
+      const response = await axios.post("https://reqres.in/api/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", response.data.token);
+      router.push("/");
+    } catch (error) {
+      setError("Wrong email or password.");
+    }
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            ref={emailRef}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            ref={passwordRef}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p className={styles.error}>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
